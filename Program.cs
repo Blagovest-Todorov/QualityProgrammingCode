6a1d69d91597f1b808c841fd219e43f9144ccc9f
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-15 13:19:29 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace QualityProgramingCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // High Wuality programming code
+
+            // --easy to read and to underastand
+            // -easy to modify and maintain
+            //correct behaviur ai all cases
+            // -we tested
+            //well architectures and design
+            //well documented, 
+            //well formated code !
+            // cood namesfor classes, mehods, variables, 
+            // strong cohesion- one method makes one thing, one class must make only one thing .- > independancy !
+            // Loose coupling---modeule to depend less on each other, dependancy to be less !!  independacy of the modules !!
+            //conventions about style of writing the code, -> Code conventions !
+            //software quality is way of thinking!
+            //minimixe the amount of complexity managing the complexity has central role in software constructon
+            //design modules and classes to reduce the complexite.
+            //devide the complex tind to more small things
+            // devide the Big problem /Task into lot of small problems/taks to solve 
+
+            // the Quality code :
+
+            //it must work as expected !,
+            //stable, no craches, bug free-works a s expected !
+            // readable, -easy to read , the name of the variables, mehtods, classes to be self-describing !
+            // understandable  -self documenting
+            // maintainble-easy to modify when  needed, 
+
+            // high quality methods --short written methods good methods names, and parameterNames, reduced complexity, improved readability 
+            // high-quality documentation --?effective comments, self documentign code-. comments in english only!
+            // well organized exception handling !!
+            // wuality code instead of good performance!
+        }
+    }
+}
